d6aa40d62d2d65d5d282abbc904cf8f17eb047ee
Gurur Sarbanoglu <gurursarbanoglu@Gururs-MacBook-Pro.local>
2017-08-22 06:57:03 +0100

    Initial commit of files

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('article').load('pages/welcome.html');
+    $('#book-list').load('pages/books-a.html');
+
+    $('aside #book-list ol li a').click(function () {
+        var pageToLoad = $(this).attr('href');
+        $('article').load('/pages/' + pageToLoad + '.html');
+        return false;
+    });
+
+    $('#alphabet-navigation a').click(function () {
+        $('#alphabet-navigation a').removeClass('selected');
+        $(this).addClass('selected');
+        var listToLoad = $(this).attr('href');
+        $('#book-list').load('pages/books-' + listToLoad + '.html');
+        return false;
+    });
+});
